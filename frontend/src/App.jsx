702a65a967a9133ef0ada3f78d9311188ed701db
Pratham-1024 <prathamgupta506@gmail.com>
2024-06-12 13:32:42 +0530

    Created Frontend Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 
+// import { Signup } from ;
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+   <>
+     <BrowserRouter>
+     <Routes>
+
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/signin" element={<Signin />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route path="/send" element={<SendMoney />}></Route>
+
+     </Routes>
+     </BrowserRouter>
+   </>
   )
 }
 
